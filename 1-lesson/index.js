3b3b161d5636eee387a258ba31b2b883ed0ccabe
Orzumand <hayotbekyoldashev0@gmail.com>
2023-03-23 17:33:28 +0500

    first lesson

---
+++ 1-lesson/index.js
@@ -0,0 +1,35 @@
+// —  JS nima?
+// —  JS qanday ishlaydi
+// —  Single Thread?
+// —  Variables - var, let, const
+// —  Data Types
+// —  Primiteve - number, bigInt, string, boolean, null, undefined, symbols
+// —  NonPrimitive - objects, arrays, functions
+// —  null vs undefined
+// —  alert(), prompt(), confirm(), confirm()
+
+// ------|----------------|-------------------|---------------------|
+
+// let text = "hello JavaScript";
+// console.log(text);
+// output: hello JavaScript
+
+// let text1;
+// console.log(text1);
+// output: undefined
+
+// console.log(text2);
+// output: ReferenceError: text2 is not defined
+// --------|----------------|---------------------|-------------|
+
+// let a = 1,
+//   b = 2,
+//   c = 3;
+
+// console.log(a, b, c);
+// output: 1 2 3
+
+// —  Primiteve - number, bigInt, string, boolean, null, undefined, symbols
+// —  NonPrimitive - object - obj, arrays, functions
+
+console.log(typeof null);
